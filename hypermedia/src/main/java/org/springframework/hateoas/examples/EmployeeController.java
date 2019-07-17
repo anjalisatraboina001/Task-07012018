747cb5911e3a7ca2f46392446f2d8f945934084c
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 20:18:22 -0600

    Update EmployeeController.java

--- hypermedia/src/main/java/org/springframework/hateoas/examples/EmployeeController.java
+++ hypermedia/src/main/java/org/springframework/hateoas/examples/EmployeeController.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.springframework.hateoas.examples;
 
 import java.util.stream.Collectors;
@@ -25,9 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author Greg Turnquist
- */
+
 @RestController
 class EmployeeController {
 
@@ -43,11 +27,7 @@ class EmployeeController {
 		this.employeeWithManagerResourceAssembler = employeeWithManagerResourceAssembler;
 	}
 
-	/**
-	 * Look up all employees, and transform them into a REST collection resource using
-	 * {@link EmployeeRepresentationModelAssembler#toCollectionModel(Iterable)}. Then return them through Spring Web's
-	 * {@link ResponseEntity} fluent API.
-	 */
+	
 	@GetMapping("/employees")
 	public ResponseEntity<CollectionModel<EntityModel<Employee>>> findAll() {
 
@@ -55,13 +35,7 @@ class EmployeeController {
 
 	}
 
-	/**
-	 * Look up a single {@link Employee} and transform it into a REST resource using
-	 * {@link EmployeeRepresentationModelAssembler#toModel(Object)}. Then return it through Spring Web's
-	 * {@link ResponseEntity} fluent API.
-	 *
-	 * @param id
-	 */
+	
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<EntityModel<Employee>> findOne(@PathVariable long id) {
 
@@ -71,12 +45,7 @@ class EmployeeController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 
-	/**
-	 * Find an {@link Employee}'s {@link Manager} based upon employee id. Turn it into a context-based link.
-	 *
-	 * @param id
-	 * @return
-	 */
+	
 	@GetMapping("/managers/{id}/employees")
 	public ResponseEntity<CollectionModel<EntityModel<Employee>>> findEmployees(@PathVariable long id) {
 

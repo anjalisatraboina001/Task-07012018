49ddf8e58072057e861fb8c91cadeebbecd775d3
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 20:24:24 -0600

    Update Supervisor.java

--- hypermedia/src/main/java/org/springframework/hateoas/examples/Supervisor.java
+++ hypermedia/src/main/java/org/springframework/hateoas/examples/Supervisor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.springframework.hateoas.examples;
 
 import lombok.Value;
@@ -23,12 +9,7 @@ import java.util.stream.Collectors;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-/**
- * Legacy representation. Contains older format of data. Fewer links because hypermedia at the time was an after
- * thought.
- *
- * @author Greg Turnquist
- */
+
 @Value
 @JsonPropertyOrder({ "id", "name", "employees" })
 class Supervisor {

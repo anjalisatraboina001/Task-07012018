4b0c82019e54449fc6793fa59c544b0c8944aa23
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 20:20:37 -0600

    Update EmployeeWithManagerResourceAssembler.java

--- hypermedia/src/main/java/org/springframework/hateoas/examples/EmployeeWithManagerResourceAssembler.java
+++ hypermedia/src/main/java/org/springframework/hateoas/examples/EmployeeWithManagerResourceAssembler.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.springframework.hateoas.examples;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
@@ -22,17 +8,11 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
-/**
- * @author Greg Turnquist
- */
+
 @Component
 class EmployeeWithManagerResourceAssembler implements SimpleRepresentationModelAssembler<EmployeeWithManager> {
 
-	/**
-	 * Define links to add to every individual {@link EntityModel}.
-	 *
-	 * @param resource
-	 */
+	
 	@Override
 	public void addLinks(EntityModel<EmployeeWithManager> resource) {
 
@@ -42,11 +22,7 @@ class EmployeeWithManagerResourceAssembler implements SimpleRepresentationModelA
 		resource.add(linkTo(methodOn(EmployeeController.class).findAllDetailedEmployees()).withRel("detailedEmployees"));
 	}
 
-	/**
-	 * Define links to add to the {@link CollectionModel} collection.
-	 *
-	 * @param resources
-	 */
+	
 	@Override
 	public void addLinks(CollectionModel<EntityModel<EmployeeWithManager>> resources) {
 

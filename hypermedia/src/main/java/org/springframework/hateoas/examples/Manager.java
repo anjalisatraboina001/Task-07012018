44704c1ed494d75c0ad115b36a2223398a738b6a
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 20:22:53 -0600

    Update Manager.java

--- hypermedia/src/main/java/org/springframework/hateoas/examples/Manager.java
+++ hypermedia/src/main/java/org/springframework/hateoas/examples/Manager.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.springframework.hateoas.examples;
 
 import lombok.Data;
@@ -29,9 +15,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * @author Greg Turnquist
- */
+
 @Data
 @Entity
 @NoArgsConstructor
@@ -40,9 +24,7 @@ class Manager {
 	@Id @GeneratedValue private Long id;
 	private String name;
 
-	/**
-	 * To break the recursive, bi-directional interface, don't serialize {@literal employees}.
-	 */
+	
 	@JsonIgnore //
 	@OneToMany(mappedBy = "manager") //
 	private List<Employee> employees = new ArrayList<>();
